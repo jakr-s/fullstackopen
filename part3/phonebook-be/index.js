0aa8896c458ae44386b8e006251512d535ca5f84
Jak <176810031+jakr-s@users.noreply.github.com>
2025-04-29 19:50:58 +0100

    📓 complete exercises 3.13.-3.14.
    - 🚧 (WIP) add MongoDB integration

--- part3/phonebook-be/index.js
+++ part3/phonebook-be/index.js
@@ -1,45 +1,22 @@
+require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
+const Person = require('./models/person')
 
 const app = express()
 
-app.use(express.json())
+let persons = []
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
 app.use(express.static('dist'))
-
-morgan.token('body', (req) => {
-  return JSON.stringify(req.body)
-})
-app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body')
-)
-
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456'
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523'
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122'
-  }
-]
-
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
+app.use(express.json())
+app.use(requestLogger)
 
 app.get('/api/info', (req, res) => {
   res.send(`
@@ -48,22 +25,16 @@ app.get('/api/info', (req, res) => {
   `)
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  const person = persons.find((person) => person.id === id)
-
-  if (person) {
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then((person) => {
     res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  persons = persons.filter((person) => person.id !== id)
-
-  res.status(204).end()
+app.get('/api/persons/:id', (req, res) => {
+  Person.findById(req.params.id).then((person) => {
+    res.json(person)
+  })
 })
 
 app.post('/api/persons', (req, res) => {
@@ -75,24 +46,32 @@ app.post('/api/persons', (req, res) => {
     })
   }
 
-  if (persons.some((person) => person.name === body.name)) {
-    return res.status(400).json({
-      error: 'name must be unique'
-    })
-  }
+  // TODO: Check for duplicate entries
 
-  const person = {
-    id: String(Math.floor(Math.random() * 10000)),
+  const person = new Person({
     name: body.name,
     number: body.number
-  }
+  })
 
-  persons = persons.concat(person)
-
-  res.json(person)
+  person.save().then((savedPerson) => {
+    res.json(savedPerson)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  persons = persons.filter((person) => person.id !== id)
+
+  res.status(204).end()
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
